8e44f40e85fc8d22dddb04bcb036ade4edde038c
rohitt24k <rohitt24k@gmail.com>
2023-09-20 17:38:28 +0530

    starting files

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,8 @@
+function Homepage() {
+  return (
+    <>
+      <h1>hey this is the homepage</h1>
+    </>
+  );
+}
+export default Homepage;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,5 @@
+function Login(){
+    return <h1>Hey this is the login page welcome</h1>
+}
+
+export default Login;
