796fd6634da638454d62b561e559d095508f3327
rohitt24k <rohitt24k@gmail.com>
2023-09-22 01:10:38 +0530

    solved error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Signin from "./signin";
-import UnivSignup from "./univSignup";
-import Choose from "./choose";
-import SignupForm from "./signup";
+import Login from "./pages/Login";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
